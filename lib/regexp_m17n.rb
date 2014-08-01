4e917057d74114471a44aab3e4d7be5da2b55a64
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-08-01 17:42:46 +0100

    solved this

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,6 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    conv_str = str.encode('UTF-8', str.encoding, invalid: :replace, undef: :replace, replace: '?')
+    conv_str =~ /\A.+\z/
   end
 end
